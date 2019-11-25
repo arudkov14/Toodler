6f2798445faae735f4df4c6f38d5395cb9b2e4f2
arudkov14 <andrey14@ru.is>
2019-11-25 12:43:16 +0000

    Mando

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hi open up App.js to start working on your app!</Text>
+      <Text>Stebbi watch Mandolorian!!</Text>
     </View>
   );
 }
