ccae67a1285b55a38635b298ce04fe13de3394e7
Olafur <olafurao16@ru.is>
2019-11-25 20:20:33 +0000

    basic frontpage and set up project structure

---
+++ src/components/Boards/Boards.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Boards = () => {
+	return(
+		<View>
+			<Text style={styles.text}> Here are some boards </Text>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+
+	text: {
+		color: '#b4c0d4',
+	}
+});
+
+export default Boards;
